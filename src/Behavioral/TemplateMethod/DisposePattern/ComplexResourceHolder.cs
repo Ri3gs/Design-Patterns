6a3a20cf755c8853818bf11a4c83fff015d98b89
Oleksandr Dobrelia <sasha.nightwish@gmail.com>
2017-11-16 21:24:26 +0200

    adds example of template method pattern inside of template method pattern adds example of extract and override adds example of template method pattern in scope of disposing

---
+++ src/Behavioral/TemplateMethod/DisposePattern/ComplexResourceHolder.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace Behavioral.TemplateMethod.DisposePattern
+{
+	public class ComplexResourceHolder : IDisposable
+	{
+		//unmanaged resource
+		private IntPtr _buffer;
+
+		//managed resource
+		private SafeHandle _handle;
+
+		public ComplexResourceHolder()
+		{
+			_buffer = AllocateBuffer();
+			_handle = new SafeWaitHandle(IntPtr.Zero, true);
+		}
+
+		protected virtual void Dispose(bool disposing)
+		{
+			//release unmanaged resources in any case
+			ReleaseBuffer(_buffer);
+
+			//method called from Dispose(), release managed resources
+			if (disposing)
+			{
+				if (_handle != null)
+				{
+					_handle.Dispose();
+				}
+			}
+		}
+
+		//called during garbage collection at unknown time (order for managed resources is unkonwn)
+		//when this is called, there is no guarantee that managed resources were not 'finalized' by their own finalizers
+		//so we need only to dispose unmanaged resources
+		~ComplexResourceHolder()
+		{
+			Dispose(false);
+		}
+
+		//this is called by user code
+		//after this call reference to 'dead' object still exists
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		private void ReleaseBuffer(IntPtr buffer)
+		{
+			throw new NotImplementedException();
+		}
+
+		private IntPtr AllocateBuffer()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class ProperComplexResourceHolder
+	{
+		//unmanaged resource
+		private IntPtr _buffer;
+
+		//managed resource
+		private SafeHandle _handle;
+
+		public ProperComplexResourceHolder()
+		{
+			_buffer = AllocateBuffer();
+			_handle = new SafeWaitHandle(IntPtr.Zero, true);
+		}
+
+		~ProperComplexResourceHolder()
+		{
+			DisposeNativeResources();
+		}
+
+		public void Dispose()
+		{
+			DisposeNativeResources();
+			DisposeManagedResources();
+			GC.SuppressFinalize(this);
+		}
+
+		protected virtual void DisposeNativeResources()
+		{
+			ReleaseBuffer(_buffer);
+		}
+
+		protected virtual void DisposeManagedResources()
+		{
+			if (_handle != null)
+				_handle.Dispose();
+		}
+
+		private void ReleaseBuffer(IntPtr buffer)
+		{
+			throw new NotImplementedException();
+		}
+
+		private IntPtr AllocateBuffer()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/Behavioral/TemplateMethod/Logs/LogFileReaderBase.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Behavioral.TemplateMethod.Logs
+{
+	//we can use template method once again for one algorith step from base (LogReader) class
+	public abstract class LogFileReaderBase : LogReader, IDisposable
+	{
+		private readonly Lazy<Stream> _stream;
+
+		protected LogFileReaderBase(string fileName)
+		{
+			_stream = new Lazy<Stream>(() => new FileStream(fileName, FileMode.Open));
+		}
+
+		protected override IEnumerable<string> ReadEntries(ref int position)
+		{
+			if (_stream.Value.Position != position)
+				_stream.Value.Seek(position, SeekOrigin.Begin);
+
+			return ReadLineByLine(_stream.Value, ref position);
+		}
+
+		protected abstract override LogEntry ParseLogEntry(string stringEntry);
+
+		private IEnumerable<string> ReadLineByLine(Stream stream, ref int position)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			if (_stream.IsValueCreated)
+			{
+				_stream.Value.Close();
+			}
+		}
+	}
+
+	//sometimes it's a good idea to extract a strategy for one step of algorithm
+	public interface ILogParser
+	{
+		LogEntry ParseLogEntry(string stringEntry);
+	}
+
+	//this is questionable, as for me
+	public abstract class LogFileReaderBase1 : LogReader, IDisposable
+	{
+		private readonly Lazy<Stream> _stream;
+		private readonly ILogParser _logParser;
+
+		protected LogFileReaderBase1(string fileName, ILogParser logParser)
+		{
+			_logParser = logParser;
+			_stream = new Lazy<Stream>(() => new FileStream(fileName, FileMode.Open));
+		}
+
+		protected override IEnumerable<string> ReadEntries(ref int position)
+		{
+			if (_stream.Value.Position != position)
+				_stream.Value.Seek(position, SeekOrigin.Begin);
+
+			return ReadLineByLine(_stream.Value, ref position);
+		}
+
+		protected override LogEntry ParseLogEntry(string stringEntry)
+		{
+			return _logParser.ParseLogEntry(stringEntry);
+		}
+
+		private IEnumerable<string> ReadLineByLine(Stream stream, ref int position)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			if (_stream.IsValueCreated)
+			{
+				_stream.Value.Close();
+			}
+		}
+	}
+
+	//how to test (extract and override)
+	public abstract class LogFileReaderBaseForTests : LogReader, IDisposable
+	{
+		private readonly Lazy<Stream> _stream;
+
+		protected LogFileReaderBaseForTests(string fileName)
+		{
+			_stream = new Lazy<Stream>(() => OpenFileStream(fileName));
+		}
+
+		//extract opening stream to virtual method
+		protected virtual Stream OpenFileStream(string fileName)
+		{
+			return new FileStream(fileName, FileMode.Open);
+		}
+
+		protected override IEnumerable<string> ReadEntries(ref int position)
+		{
+			if (_stream.Value.Position != position)
+				_stream.Value.Seek(position, SeekOrigin.Begin);
+
+			return ReadLineByLine(_stream.Value, ref position);
+		}
+
+		protected abstract override LogEntry ParseLogEntry(string stringEntry);
+
+		private IEnumerable<string> ReadLineByLine(Stream stream, ref int position)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Dispose()
+		{
+			if (_stream.IsValueCreated)
+			{
+				_stream.Value.Close();
+			}
+		}
+	}
+
+	//and create fake one
+	public class FakeLogFileReader : LogFileReaderBaseForTests
+	{
+		private readonly MemoryStream _mockStream;
+
+		public FakeLogFileReader(MemoryStream mockStream) : base(string.Empty)
+		{
+			_mockStream = mockStream;
+		}
+
+		protected override Stream OpenFileStream(string fileName)
+		{
+			return _mockStream;
+		}
+
+		protected override LogEntry ParseLogEntry(string stringEntry)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
