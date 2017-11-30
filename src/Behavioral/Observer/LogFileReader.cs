4f362f3ddee2788f01732a677fdd1e0c1a3cc486
Oleksandr Dobrelia <sasha.nightwish@gmail.com>
2017-11-30 20:04:07 +0200

    adds simplest observer example

---
+++ src/Behavioral/Observer/LogFileReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Behavioral.Observer
+{
+	public class LogFileReader : IDisposable
+	{
+		private readonly string _logFile;
+		private readonly Action<string> _logEntrySubscriber;
+		private static readonly TimeSpan CheckFileInterval = TimeSpan.FromSeconds(5);
+		private readonly Timer _timer;
+
+		public LogFileReader(string logFile, Action<string> logEntrySubscriber)
+		{
+			_logFile = logFile;
+			_logEntrySubscriber = logEntrySubscriber;
+			_timer = new Timer(CheckFile, new object(), CheckFileInterval, CheckFileInterval);
+		}
+
+		public void Dispose()
+		{
+			_timer.Dispose();
+		}
+
+		private void CheckFile(object state)
+		{
+			foreach (var logEntry in ReadNewLogEntries())
+			{
+				_logEntrySubscriber(logEntry);
+			}
+		}
+
+		private IEnumerable<string> ReadNewLogEntries()
+		{
+			// Read new records from file
+			// that appeared from last read
+			return new List<string>();
+		}
+	}
+}
