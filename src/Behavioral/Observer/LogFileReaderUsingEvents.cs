1d4f1e0107f72ad7edac5f8ebf7696abf05fcad8
Oleksandr Dobrelia <sasha.nightwish@gmail.com>
2017-12-02 17:47:37 +0200

    adds push/pull example to Observer example with events

--- src/Behavioral/Observer/LogFileReaderUsingEvents.cs
+++ src/Behavioral/Observer/LogFileReaderUsingEvents.cs
@@ -7,15 +7,16 @@ namespace Behavioral.Observer
 	public class LogFileReaderUsingEvents : IDisposable
 	{
 		private static readonly TimeSpan CheckFileInterval = TimeSpan.FromSeconds(5);
-		private readonly string _logFileName;
 		private readonly Timer _timer;
 
 		public LogFileReaderUsingEvents(string logFileName)
 		{
-			_logFileName = logFileName;
+			LogFileName = logFileName;
 			_timer = new Timer(CheckFile, new object(), CheckFileInterval, CheckFileInterval);
 		}
 
+		// but observers can 'pull' additional information
+		public readonly string LogFileName;
 		public event EventHandler<LogEntryEventArgs> OnNewLogEntry;
 
 		private void CheckFile(object state)
@@ -28,6 +29,7 @@ namespace Behavioral.Observer
 
 		private void RaiseNewLogEntry(string logEntry)
 		{
+			// we 'push' only necessary information
 			EventHandler<LogEntryEventArgs> handler = OnNewLogEntry;
 			if (handler != null)
 				handler(this, new LogEntryEventArgs{ LogEntry = logEntry });
@@ -41,6 +43,22 @@ namespace Behavioral.Observer
 		public void Dispose()
 		{
 			_timer.Dispose();
+		}
+	}
+
+	public class LogForwarder
+	{
+		public LogForwarder(LogFileReaderUsingEvents logFileReaderUsingEvents)
+		{
+			logFileReaderUsingEvents.OnNewLogEntry += HandleNewEntry;
+		}
+
+		private void HandleNewEntry(object sender, LogEntryEventArgs logEntryEventArgs)
+		{
+			var logEntry = logEntryEventArgs.LogEntry;
+			var logFileName = ((LogFileReaderUsingEvents) sender).LogFileName;
+
+			// process logEntry considering logFileName
 		}
 	}
 }
