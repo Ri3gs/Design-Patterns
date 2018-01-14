0c2447f9773bc0921194225740e2c4445f7b87ed
Oleksandr Dobrelia <sasha.nightwish@gmail.com>
2018-01-14 15:42:56 +0200

    moves files around adds some unit tests and related changes to Composite pattern

---
+++ Structural.Test/RejectOldEntriesWithLowSeverityTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Structural.Composite;
+
+namespace Structural.Test
+{
+	[TestClass]
+	public class RejectOldEntriesWithLowSeverityTest
+	{
+		private LogImportRule _rule;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_rule = LogRuleFactory.RejectOldEntriesWithLowSeverity(TimeSpan.FromDays(7));
+		}
+
+		[TestMethod]
+		public void ImportsExceptionEntry()
+		{
+			// Arrange
+			var logEntry = new ExceptionLogEntry();
+
+			// Assert
+			Assert.IsTrue(_rule.ShouldImport(logEntry));
+		}
+
+		[TestMethod]
+		public void RejectsOldEntryWithLowSeverity()
+		{
+			// Arrange
+			var logEntry = new SimpleLogEntry { EntryDateTime = DateTime.Now.AddDays(-10) };
+
+			// Assert
+			Assert.IsFalse(_rule.ShouldImport(logEntry));
+		}
+
+		[TestMethod]
+		public void ImportsOldEntryWithHighSeverity()
+		{
+			// Arrange
+			var logEntry = new SimpleLogEntry { EntryDateTime = DateTime.Now.AddDays(-10), Severity = Severity.Critical };
+
+			// Assert
+			Assert.IsTrue(_rule.ShouldImport(logEntry));
+		}
+
+		[TestMethod]
+		public void ImportsEntryWithinSpecifiedTimeSpan()
+		{
+			// Arrange
+			var logEntry = new SimpleLogEntry { EntryDateTime = DateTime.Now.AddDays(-5) };
+
+			// Assert
+			Assert.IsTrue(_rule.ShouldImport(logEntry));
+		}
+	}
+}
