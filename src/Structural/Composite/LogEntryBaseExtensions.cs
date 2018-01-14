7631794b65bc072dc78aa0fbe2cd4ea52d105c7c
Oleksandr Dobrelia <sasha.nightwish@gmail.com>
2018-01-14 15:31:17 +0200

    modifies how rules are composed, moves structural project to src folder

---
+++ src/Structural/Composite/LogEntryBaseExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Structural.Composite
+{
+	public static class LogEntryBaseExtensions
+	{
+		//or OccurredWithin
+		public static bool Within(this LogEntryBase logEntry, TimeSpan period) =>
+			logEntry.TimeSpanFromEntryDateToNow() <= period;
+
+		public static bool OlderThan(this LogEntryBase logEntry, TimeSpan period) =>
+			logEntry.TimeSpanFromEntryDateToNow() > period;
+
+		public static TimeSpan TimeSpanFromEntryDateToNow(this LogEntryBase logEntry)
+			=> DateTime.Now - logEntry.EntryDateTime;
+	}
+}
